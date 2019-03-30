455a97036a4c4988d57e91387d2b229fc0a57830
stevenmartinezanalyst <stevenmartinezanalyst@gmail.com>
2019-03-30 14:00:31 -0700

    Added CSV Creation code and specified fields to be written to the CSV.

---
+++ StarredPlaces.py
@@ -0,0 +1,21 @@
+#Written by Steven Martinez
+#Convert Google Saved Places GeoJSON to CSV
+import json
+import csv
+#GeoJSON in same directory as script
+filename = "Saved Places.json"
+file = open(filename, "r")
+json_parsed = json.load(file)
+data = json_parsed.get("features")
+#Write a new CSV in the same directory
+csv_file = open('SavedPlaces.csv', 'w')
+csvwriter = csv.writer(csv_file)
+count = 0
+for i in data:
+	if count == 0:
+		#Establish the headers to be written to the CSV
+		header = ["Title","Address","Country_Code","Long","Lat","URL"]
+		csvwriter.writerow(header)
+		count += 1
+	csvwriter.writerow([i.get('properties').get('Title'),i.get('properties').get('Location').get('Address'),i.get('properties').get('Location').get('Country Code'),i.get('geometry').get('coordinates')[0],i.get('geometry').get('coordinates')[1],i.get('properties').get('Google Maps URL')])
+csv_file.close()
