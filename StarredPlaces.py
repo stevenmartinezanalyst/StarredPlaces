5679b63752ed38812ea3614be0ea962cb75c241e
stevenmartinezanalyst <stevenmartinezanalyst@gmail.com>
2019-04-04 19:46:33 -0700

    Modified Write to CSV code.

--- StarredPlaces.py
+++ StarredPlaces.py
@@ -8,14 +8,13 @@ file = open(filename, "r")
 json_parsed = json.load(file)
 data = json_parsed.get("features")
 #Write a new CSV in the same directory
-csv_file = open('SavedPlaces.csv', 'w')
-csvwriter = csv.writer(csv_file)
-count = 0
-for i in data:
-	if count == 0:
-		#Establish the headers to be written to the CSV
-		header = ["Title","Address","Country_Code","Long","Lat","URL"]
-		csvwriter.writerow(header)
-		count += 1
-	csvwriter.writerow([i.get('properties').get('Title'),i.get('properties').get('Location').get('Address'),i.get('properties').get('Location').get('Country Code'),i.get('geometry').get('coordinates')[0],i.get('geometry').get('coordinates')[1],i.get('properties').get('Google Maps URL')])
-csv_file.close()
+with open('SavedPlaces.csv','w') as csv_file:
+	csvwriter = csv.writer(csv_file)
+	count = 0
+	for i in data:
+		if count == 0:
+			#Establish the headers to be written to the CSV
+			header = ["Title","Address","Country_Code","Long","Lat","URL"]
+			csvwriter.writerow(header)
+			count += 1
+		csvwriter.writerow([i.get('properties').get('Title'),i.get('properties').get('Location').get('Address'),i.get('properties').get('Location').get('Country Code'),i.get('geometry').get('coordinates')[0],i.get('geometry').get('coordinates')[1],i.get('properties').get('Google Maps URL')])
